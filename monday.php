434d3cbf7d4ba9e8403b7985fb4ce7e5249508fe
Thomas Aldaitz <thomas.aldaitz@gmail.com>
2023-03-27 08:52:52 +0200

    publish the monday kata

---
+++ monday.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 1 - Créer un formulaire de connection avec deux champs :
+ *      - email : champ texte obligatoire qui doit vérifier 
+ *                  que le format email est respecté
+ *      - mot de passe : champ de mot de passe obligatoire
+ * 
+ * 2 - Vérifiez côté server que les deux champs sont bien renseignés 
+ *     sinon afficher un message d'erreur
+ * 
+ * 3 - Si l'email est "admin@wcs.com" et que le mot de passe saisie est "admin123"
+ *      alors afficher "Bravo, vous êtes identifié."
+ *     Sinon réafficher le formulaire avec le message "Désolé, vous n'êtes pas super 
+ * Administrateur."
+ */
